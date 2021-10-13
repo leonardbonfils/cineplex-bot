ec6aa891c4dc49be226ed2447e562316bbc133eb
EK230948230598 <6782987+EK230948230598@users.noreply.github.com>
2021-10-13 07:02:57 -0400

    Fixed f-string format

--- Cineplex.py
+++ Cineplex.py
@@ -103,7 +103,7 @@ while(ticketsOnSale == False):
         print(Fore.BLUE + f"{nowFormatted}: " + Fore.LIGHTGREEN_EX + "Tickets are not on sale yet.")
     else:
         ticketsOnSale = True
-        print(Fore.RED + f"TICKETS NOW ON SALE! - " + Fore.MAGENTA + "Released at {nowFormatted}")
+        print(Fore.RED + f"TICKETS NOW ON SALE! - " + Fore.MAGENTA + "Released at f{nowFormatted}")
 
     time.sleep(60)
     
